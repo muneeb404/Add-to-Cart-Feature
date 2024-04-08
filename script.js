d8fc31f47586a657b2fc818754b1479d05f606ef
muneeb404 <muneeb16.jan@gmail.com>
2024-04-08 07:56:05 +0500

    updated

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 var products = [
-    {name:"Green Clay", headline:"Beautiful Clay Cushions", price:"900", image:"https://images.unsplash.com/photo-1712431028580-049087ee59d8?q=80&w=1504&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
-    {name:"Diamond Button", headline:"Diamond in the black", price:"7800", image:"https://images.unsplash.com/photo-1515526764876-0f3b298ee4ac?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
-    {name:"Comfortable Chair", headline:"Black Chair in BlueBack", price:"1000", image:"https://images.unsplash.com/photo-1592078615290-033ee584e267?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
+    {name:"Green Clay", headline:"Beautiful Clay Cushions", price:"1100", image:"https://images.unsplash.com/photo-1712431028580-049087ee59d8?q=80&w=1504&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {name:"Diamond Button", headline:"Qeemti Pathar", price:"7800", image:"https://images.unsplash.com/photo-1515526764876-0f3b298ee4ac?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {name:"Comfortable Chair", headline:"Black Chair in BlueBack", price:"1000", image:"https://images.unsplash.com/photo-1592078615290-033ee584e267?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {name:"Hafiz ka Halwa", headline:"Desi Sohan Halwa", price:"9000", image:"https://images.unsplash.com/photo-1517244683847-7456b63c5969?q=80&w=1376&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {name:"Coffee Table", headline:"Table ha yeh", price:"890", image:"https://images.unsplash.com/photo-1533090481720-856c6e3c1fdc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHRhYmxlfGVufDB8fDB8fHww"}
 ];
 var popular = [
-    {name:"Green Clay", headline:"Beautiful Clay Cushions", price:"900", image:"https://images.unsplash.com/photo-1712431028580-049087ee59d8?q=80&w=1504&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
-    {name:"Diamond Button", headline:"Diamond in the black", price:"7800", image:"https://images.unsplash.com/photo-1515526764876-0f3b298ee4ac?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {name:"Coffee Table", headline:"Table ha yeh", price:"890", image:"https://images.unsplash.com/photo-1533090481720-856c6e3c1fdc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHRhYmxlfGVufDB8fDB8fHww"},
+    {name:"Green Clay", headline:"Beautiful Clay Cushions", price:"1100", image:"https://images.unsplash.com/photo-1712431028580-049087ee59d8?q=80&w=1504&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {name:"Diamond Button", headline:"Qeemti Pathar", price:"7800", image:"https://images.unsplash.com/photo-1515526764876-0f3b298ee4ac?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {name:"Hafiz ka Halwa", headline:"Desi Sohan Halwa", price:"9000", image:"https://images.unsplash.com/photo-1517244683847-7456b63c5969?q=80&w=1376&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
     {name:"Comfortable Chair", headline:"Black Chair in BlueBack", price:"1000", image:"https://images.unsplash.com/photo-1592078615290-033ee584e267?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
 ];
 var cart = [];
@@ -43,7 +47,7 @@ const addPictures = () =>{
                     <h3 class="font-semibold opacity-20">${product.headline}</h3>
                     <h4 class="font-semibold mt-2">&dollar; ${product.price}</h4>
                 </div>
-                <button data-index ="${index}" class=" add w-10 h-10 rounded-full bg-[#4A4563] text-yellow-400"><i
+                <button data-index ="${index}" class=" add w-10 h-10 rounded-full bg-[#4A4563] text-yellow-400 mt-7"><i
                         data-index ="${index}" class=" add ri-add-line"></i></button>
             </div>
         </div>
@@ -74,7 +78,7 @@ const showCart = ()=>{
             </div>
             <div>
                 <h3 class="font-semibold">${prod.name}</h3>
-                <h5 class="text-sm font-semibold opacity-80">${prod.price}</h5>
+                <h5 class="text-sm font-semibold opacity-80">&dollar; ${prod.price}</h5>
             </div>
         </div>`;
         })
